d0cb2c39fd73ce91b9d2a998a4b46d99451b4a55
David Kahle <david.kahle@gmail.com>
2019-11-02 10:05:50 -0500

    commit installs.R

---
+++ installs.R
@@ -0,0 +1,6 @@
+install.packages(c("devtools", "tidyverse", "gmp"))
+devtools::install_github("dkahle/mpoly")
+devtools::install_github("coneill-math/m2r")
+devtools::install_github("dkahle/latte")
+devtools::install_github("dkahle/bertini")
+devtools::install_github("dkahle/algstat")
