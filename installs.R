991a706fbef445f24c0984d24b0ba90d23e0cfe4
David Kahle <david.kahle@gmail.com>
2019-11-02 10:21:41 -0500

    add plotly to installs

--- installs.R
+++ installs.R
@@ -1,4 +1,4 @@
-install.packages(c("devtools", "tidyverse", "gmp"))
+install.packages(c("devtools", "tidyverse", "gmp", "plotly"))
 devtools::install_github("dkahle/mpoly")
 devtools::install_github("coneill-math/m2r")
 devtools::install_github("dkahle/latte")
